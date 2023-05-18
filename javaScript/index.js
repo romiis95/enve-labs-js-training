2073f55edf8bdae4024f9c8d2f750c8dbb4ab2d1
romiis95 <romiirodriguez89@gmail.com>
2023-05-18 17:31:39 -0300

    se termino la app del clima

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,37 +1,37 @@
-const APY_KEY ='56a7c4d5b0b6617f7099fae1b01e8a82'
+const APP_KEY ='56a7c4d5b0b6617f7099fae1b01e8a82';
 
-const fechdata = position => {
+const fetchdata = position => {
+    const {latitude, longitude} = position.coords;
+    // fetch(`http://api.openweathermap.org/data/2.5/weather?uits=metric&lat=${latitude}&lon=${longitude}&units=metric&appid=${APP_KEY}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${APP_KEY}`)
     
-    const {latitud, longitude} = position.coords
-    fetch(`http://api.openweathermap.org/data/2.5/weather?uits=metric&lat=${latitude}&lon=${longitude}&units=metric&appid=${APP_ID}`)
-        .then(Response =>console.log(Response));
-        console.log(position)
+        // .then(Response =>console.log(Response));
+        // console.log(position)
         .then(response => response.json())
-        .then(data => console.log (data))
-}
+        .then(data => setWeatherData(data));
+console.log('HOLA');
 
+    }
+
+    
 const setWeatherData = data => {
     const weatherData = {
-        location: data?.name || "",
-        description: data?.weather[0]?.main || "",
-        humidity: data?.main?.humidity || 0,
-        pressure: data?.main?.pressure || 0,
-        temperature: Math.floor(data?.main?.temp) || 0,
+        location: data.name,
+        description: data.weather[0].main,
+        humidity: data.main.humidity,
+        pressure: data.main.pressure,
+        temperature: Math.floor(data.main.temp),
         date: getDate(),
-    };
+    }
     
     // Resto del código...
-};
-
-
-function setTextContent(key, value) {
-    const element = document.getElementById(key);
-    if (element) {
-      element.textContent = value;
-        }
-    }
+    Object.keys(weatherData).forEach( key => {
+    setTextContent(key, weatherData[key]);
+    });
+    
     cleanUp();
 
+};
 
 const cleanUp = () => {
     let container = document.getElementById('container');
@@ -46,13 +46,10 @@ const getDate = () => {
     return `${date.getDate()}-${ ('0' + (date.getMonth() + 1)).slice(-2)}-${date.getFullYear()}`;
 }
 
-Object.keys(weatherData).forEach(key => {
-    setTextContent(key, weatherData[key]);
-});
+const setTextContent = (element, text) => {
+    document.getElementById(element).textContent = text;
+}
 
-    
-    
-    
-const onload =() => {
-        navigator.geolocation.getCurrentPosition(log)
-    }
+const onload = () => {
+    navigator.geolocation.getCurrentPosition(fetchdata);
+}
